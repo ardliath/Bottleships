57d2bb093ee3542e00de356781de0f04f2a00c6c
Adam Griffiths <Adam.Griffiths@callcreditgroup.com>
2020-05-05 16:19:21 +0100

    Creating Nelson

--- Code/Bottleships/Bottleships/AI/Nelson.cs
+++ Code/Bottleships/Bottleships/AI/Nelson.cs
@@ -10,11 +10,22 @@ namespace Bottleships.AI
 {
     public class Nelson : ICaptain
     {
+        public class Find
+        {
+            public ShotResult OriginalHittingShot { get; set; }
+            
+            public Stack<Shot> UpcomingShots { get; set; }
+
+            public Find()
+            {
+                this.UpcomingShots = new Stack<Shot>();
+            }
+        }
+
 
         public void StartGameNotification(GameStartNotification gameStartNotification)
         {
             this.ShotHistory = new List<Shot>();
-            this.LastSearchShotTaken = new Dictionary<EnemyFleetInfo, Shot>();
         }
 
 
@@ -68,112 +79,188 @@ namespace Bottleships.AI
 
         public List<Shot> ShotHistory { get; set; }
 
+        public string LastHittMe { get; set; }
+
         public IEnumerable<Shot> GetShots(IEnumerable<EnemyFleetInfo> enemyFleetInfo, int numberOfShots)
         {
-            var shotsToFire = new List<Shot>();
-            var weakestFleet = enemyFleetInfo
-                .Where(f => f.NumberOfAfloatShipts > 0)
-                .OrderByDescending(f => f.NumberOfAfloatShipts)
-                .FirstOrDefault();
-
-            
-            if(!LastSearchShotTaken.ContainsKey(weakestFleet))
+            if (this.SearchShots == null)
             {
-                var openingShot = new Shot
+                CreateSearchShots(enemyFleetInfo);
+            }
+            if(LastHittMe != null)
+            {
+                // if the person who last shot at me has no ships then go back to the weakest
+                var shooter = enemyFleetInfo.Single(s => s.Name == LastHittMe);
+                if(shooter.NumberOfAfloatShipts == 0)
                 {
-                    FleetName = weakestFleet.Name,
-                    Coordinates = new Coordinates
-                    {
-                        X = 0,
-                        Y = 0
-                    }
-                };
-                LastSearchShotTaken[weakestFleet] = openingShot;
-                shotsToFire.Add(openingShot);
+                    LastHittMe = null;
+                }
             }
 
-            while(shotsToFire.Count < numberOfShots)
-            {                
-                var newShot = GetNextShot(LastSearchShotTaken[weakestFleet]);
-                shotsToFire.Add(newShot);
-                LastSearchShotTaken[weakestFleet] = newShot;
+            var shotsToFire = new List<Shot>();
+
+            string target = LastHittMe;
+            if (target == null)
+            {
+                target = enemyFleetInfo
+                    .Where(f => f.NumberOfAfloatShipts > 0)
+                    .OrderByDescending(f => f.NumberOfAfloatShipts)
+                    .FirstOrDefault()
+                    ?.Name;
+            }
+
+            // see if we've got any finds first
+            foreach (var find in Finds)
+            {
+                while (shotsToFire.Count < numberOfShots && find.UpcomingShots.Any())
+                {
+                    var nextShot = find.UpcomingShots.Pop();
+                    shotsToFire.Add(nextShot);
+                }
+            }
+
+            // then fill up with searches
+            for (int i = shotsToFire.Count; i < numberOfShots; i++)
+            {
+                if (SearchShots.ContainsKey(target)
+                    && SearchShots[target].Any())
+                {
+                    shotsToFire.Add(SearchShots[target].Pop());
+                }
             }
 
             return shotsToFire;
         }
-
-        private Shot GetNextShot(Shot lastShot)
+        private void CreateSearchShots(IEnumerable<EnemyFleetInfo> fleets)
         {
-            var evenSquares = lastShot.Coordinates.Y % 2 == 0;
-            int y;
-            if(lastShot.Coordinates.X >= 8)
-            {
-                y = Math.Min(lastShot.Coordinates.Y + 1, 9);
-            }
-            else
-            {
-                y = lastShot.Coordinates.Y;
-            }
+            SearchShots = new Dictionary<string, Stack<Shot>>();
+            this.Finds = new List<Find>();
+            Stack<Shot> searchShots;
 
-            int x;
-            if(y == lastShot.Coordinates.Y)
+            foreach (var fleet in fleets)
             {
-                x = lastShot.Coordinates.X + 2;
-            }
-            else
-            {
-                x = 0 + (evenSquares ? 0 : 1);
-            }
+                searchShots = new Stack<Shot>();
 
-            return new Shot
-            {
-                FleetName = lastShot.FleetName,
-                Coordinates = new Coordinates
+                for (int y = 0; y < 10; y++)
                 {
-                    X = x,
-                    Y = y
-                }
-            };
-        }
-
-        public Dictionary<EnemyFleetInfo, Shot> LastSearchShotTaken { get; set; }
-
-        public IEnumerable<Shot> GetShotsNotTaken(IEnumerable<EnemyFleetInfo> enemyFleetInfo)
-        {
-            var allShots = new List<Shot>();
-            var fleetsToShootAt = enemyFleetInfo.Where(f => f.NumberOfAfloatShipts > 0);
-            foreach (var enemy in fleetsToShootAt)
-            {
-                for (int x = 0; x < 10; x++)
-                {
-                    for (int y = 0; y < 10; y++)
+                    for (int x = 0; x < 5; x++)
                     {
-                        allShots.Add(new Shot
+                        searchShots.Push(new Shot
                         {
+                            FleetName = fleet.Name,
                             Coordinates = new Coordinates
                             {
-                                X = x,
+                                X = (x * 2) + (y % 2 == 0 ? 0 : 1),
                                 Y = y
-                            },
-                            FleetName = enemy.Name
+                            }
                         });
                     }
                 }
+
+                SearchShots[fleet.Name] = searchShots;
             }
-
-
-            return allShots.Except(this.ShotHistory);
         }
+
+        Dictionary<string, Stack<Shot>> SearchShots { get; set; }
+
+        public List<Find> Finds { get; set; }
+
+
 
         public void RespondToShots(IEnumerable<ShotResult> results)
         {
-            // Doesn't care if we've hit something, not smart enough for that
+            foreach (var result in results)
+            {
+                if (result.WasAHit) // if we've hit
+                {
+                    if (result.WasASink) // and sunk
+                    {
+                        foreach (var find in Finds.Where(f => f.OriginalHittingShot.Equals(result.Shot))) // then cancel all other shots aiming from that find
+                        {
+                            this.Finds.Remove(find);
+                        }
+                    }
+
+
+                    var originatingShot = this.Finds.SingleOrDefault(f => f.OriginalHittingShot.Equals(result.Shot));
+                    if (originatingShot == null) // it was a searching shot which hit
+                    {
+                        var find = new Find // create a new find
+                        {
+                            OriginalHittingShot = result
+                        };
+
+                        this.Finds.Add(find);
+
+                        // with four upcoming shots which spiral out from it
+                        find.UpcomingShots.Push(new Shot
+                        {
+                            FleetName = result.Shot.FleetName,
+                            Coordinates = new Coordinates { X = result.Shot.Coordinates.X + 1, Y = result.Shot.Coordinates.Y }
+                        });
+                        find.UpcomingShots.Push(new Shot
+                        {
+                            FleetName = result.Shot.FleetName,
+                            Coordinates = new Coordinates { X = result.Shot.Coordinates.X - 1, Y = result.Shot.Coordinates.Y }
+                        });
+                        find.UpcomingShots.Push(new Shot
+                        {
+                            FleetName = result.Shot.FleetName,
+                            Coordinates = new Coordinates { X = result.Shot.Coordinates.X, Y = result.Shot.Coordinates.Y + 1 }
+                        });
+                        find.UpcomingShots.Push(new Shot
+                        {
+                            FleetName = result.Shot.FleetName,
+                            Coordinates = new Coordinates { X = result.Shot.Coordinates.X, Y = result.Shot.Coordinates.Y - 1 }
+                        });
+                    }
+                    else // if we do know where it came from
+                    {
+                        // create another shot extending in that direction
+                        if(result.Shot.Coordinates.X > originatingShot.OriginalHittingShot.Shot.Coordinates.X) // if it's a shot to the right
+                        {
+                            originatingShot.UpcomingShots.Push(new Shot
+                            {
+                                FleetName = result.Shot.FleetName,
+                                Coordinates = new Coordinates { X = result.Shot.Coordinates.X + 1, Y = result.Shot.Coordinates.Y }
+                            });
+                        }
+
+                        if (result.Shot.Coordinates.X < originatingShot.OriginalHittingShot.Shot.Coordinates.X) // if it's a shot to the left
+                        {
+                            originatingShot.UpcomingShots.Push(new Shot
+                            {
+                                FleetName = result.Shot.FleetName,
+                                Coordinates = new Coordinates { X = result.Shot.Coordinates.X - 1, Y = result.Shot.Coordinates.Y }
+                            });
+                        }
+
+                        if (result.Shot.Coordinates.Y > originatingShot.OriginalHittingShot.Shot.Coordinates.Y)
+                        {
+                            originatingShot.UpcomingShots.Push(new Shot
+                            {
+                                FleetName = result.Shot.FleetName,
+                                Coordinates = new Coordinates { X = result.Shot.Coordinates.X, Y = result.Shot.Coordinates.Y + 1 }
+                            });
+                        }
+
+                        if (result.Shot.Coordinates.Y < originatingShot.OriginalHittingShot.Shot.Coordinates.Y)
+                        {
+                            originatingShot.UpcomingShots.Push(new Shot
+                            {
+                                FleetName = result.Shot.FleetName,
+                                Coordinates = new Coordinates { X = result.Shot.Coordinates.X, Y = result.Shot.Coordinates.Y - 1 }
+                            });
+                        }
+                    }
+                }
+            }
         }
 
         public void NotifyOfBeingHit(IEnumerable<HitNotification> hits)
         {
-
+            // swap to shoot at whoever is shooting at me
+            LastHittMe = hits.LastOrDefault()?.Shooter;
         }
-
     }
 }
